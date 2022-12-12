6c7765467cfe9de7a33a85c4f232a40747cddee0
John Rey <adioskid@mail.ru>
2022-12-13 01:23:03 +0300

    Auto

--- src/Auto.java
+++ src/Auto.java
@@ -5,17 +5,28 @@ public class Auto {
 
 
     Auto(String brand, String model, int year, String country, String color, double engineVolume) {
+
+        String def = "default";
+        if (brand == null || brand.isBlank()) brand = def;
+        if (model == null || model.isBlank()) model = def;
+        if (country == null || country.isBlank()) country = def;
+        if (color == null || color.isBlank()) color = "белого";
+
+        if (engineVolume <= 0) engineVolume = 1.5;
+        if (year <= 0) year = 2000;
+
         this.brand = brand;
         this.model = model;
         this.year = year;
         this.country = country;
         this.color = color;
         this.engineVolume = engineVolume;
+
+
     }
 
     @Override
     public String toString() {
-        return brand + " " + model + ", " + year + " года выпуска, сборка в " + country + ", " + color + " цвета, объем двигателя — " +
-                engineVolume + " л.";
+        return brand + " " + model + ", " + year + " года выпуска, сборка в " + country + ", " + color + " цвета, объем двигателя — " + engineVolume + " л.";
     }
 }
--- src/Launcher.java
+++ src/Launcher.java
@@ -1,6 +1,6 @@
 public class Launcher {
     public static void main(String[] args) {
-        Auto lada = new Auto("Lada", "Granta", 2015, "России", "желтого", 1.7);
+        Auto lada = new Auto(null, " ", 0, "", null, -1);
         Auto audi = new Auto("Audi", "A8 50 L TDI quattro", 2020, "Германии", "черного", 3);
         Auto bmw = new Auto("BMW", "Z8", 2021, "Германии", "черного", 3);
         Auto kia = new Auto("Kia", "Sportage 4-го поколения", 2018, "Южной Корее", "красного", 2.4);
