94c085c400b6b0840d3f50178576484f7a92636a
John Rey <adioskid@mail.ru>
2022-12-16 03:28:17 +0300

    auto

--- src/transport/Car.java
+++ src/transport/Car.java
@@ -6,10 +6,12 @@ public class Car {
     private final int year, numSeats;
     private double engineVolume;
 
-    private boolean rubber;
+    private boolean tire;
 
 
-    public Car(String brand, String model, double engineVolume, String transmission, int year, String country, String color, String bodyType, int numSeats) {
+    public Car(String brand, String model, double engineVolume, String transmission, int year,
+               String country, String color, String bodyType, int numSeats, String regNumber) {
+
 
         String def = "default";
         if (brand == null || brand.isBlank()) brand = def;
@@ -23,6 +25,8 @@ public class Car {
         if (transmission == null || transmission.isBlank()) transmission = "механическая";
         this.transmission = transmission;
         if (bodyType == null || bodyType.isBlank()) bodyType = def;
+        if (regNumber == null || regNumber.isBlank()) regNumber = "<<номер неизвестен>>";
+        this.regNumber = regNumber;
         this.bodyType = bodyType;
         if (engineVolume <= 0) engineVolume = 1.5;
         this.engineVolume = engineVolume;
@@ -31,6 +35,47 @@ public class Car {
         if (numSeats <= 0) numSeats = 5;
         this.numSeats = numSeats;
 
+    }
+
+
+    private String getTire(boolean tire) {
+        if (tire)
+            return "летнии";
+        return "зимнии";
+    }
+
+    public void setTire(int mount) {
+        if (mount < 1 || mount > 12) {
+            System.out.println("Введите правильное число месяцев!");
+        } else if (mount > 3 && mount < 12) {
+            tire = true;
+        }
+    }
+
+
+    private boolean checkRegNum(String regNumber) {
+        boolean bool = true;
+        char check;
+        int count = 0;
+        while (count < regNumber.length() && bool) {
+            check = regNumber.charAt(count);
+
+            if (!Character.isDigit(check) && (count > 5 || count <= 3 && count != 0)) {
+                bool = false;
+            }
+            if ((check != 'а' && check != 'в' && check != 'е' && check != 'к' &&
+                    check != 'м' && check != 'н' && check != 'о' && check != 'р' &&
+                    check != 'с' && check != 'т' && check != 'у' && check != 'х') && (count == 0 || count == 4 || count == 5)) {
+                bool = false;
+            }
+            count++;
+        }
+        return bool;
+    }
+
+    public void setTire(String mount) {
+        if (mount.equals("летнии"))
+            tire = true;
     }
 
     public String getBrand() {
@@ -75,13 +120,6 @@ public class Car {
         this.transmission = transmission;
     }
 
-    public String getRegNumber() {
-        return regNumber;
-    }
-
-    public void setRegNumber(String regNumber) {
-        this.regNumber = regNumber;
-    }
 
     public double getEngineVolume() {
         return engineVolume;
@@ -92,18 +130,23 @@ public class Car {
         this.engineVolume = engineVolume;
     }
 
-    public boolean isRubber() {
-        return rubber;
+    public String getRegNumber() {
+        if (!checkRegNum(regNumber))
+            return "<<Ошибка в регистрационном номере>>";
+        return regNumber;
     }
 
-    public void setRubber(boolean rubber) {
-        this.rubber = rubber;
+    public void setRegNumber(String regNumber) {
+        if (regNumber == null || regNumber.isBlank()) regNumber = "<<номер неизвестен>>";
+        this.regNumber = regNumber;
     }
+
 
     @Override
     public String toString() {
-        return "Марка: " + brand + "\nМодель: " + model + "\nОбъем двигателя: " + engineVolume + " л." + "\nТрансмиссия: " + transmission + "\nГод производства: " + year + "\nСтрана сборки: "
-                + country + "\nЦвет: " + color + "\nТип кузова: "
-                + bodyType + "\nКоличество мест: " + numSeats;
+        return "Марка: " + brand + "\nМодель: " + model + "\nОбъем двигателя: " + engineVolume + " л." +
+                "\nТрансмиссия: " + transmission + "\nГод производства: " + year + "\nСтрана сборки: " +
+                country + "\nЦвет: " + color + "\nТип кузова: " + bodyType + "\nКоличество мест: " + numSeats +
+                "\nУстановлены шины: " + getTire(tire) + "\nРегистрационный номер: " + getRegNumber();
     }
 }
