c65ae3356d98b439e133af12c78f2d593d009c75
John Rey <adioskid@mail.ru>
2022-12-18 01:26:34 +0300

    auto

--- src/transport/Car.java
+++ src/transport/Car.java
@@ -131,7 +131,7 @@ public class Car {
             result = checkDateIns / 365;
             mounts = result * 12;
             days = (checkDateIns <= 3||mounts==0) ? checkDateIns : (mounts - (int) mounts) * 31;
-            /*years = (int) result;*/
+            years = (int) result;
             if (checkDateIns <= 0) {
                 return "действие страхового полиса окончено";
             } else {
